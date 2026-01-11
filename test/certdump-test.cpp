19451fb66d13652aea8abe506e9012dfc595fe55
Viliam Lejcik <lejcik@gmail.com>
2026-01-11 01:46:09 +0100

    used own gtest main file

---
+++ test/certdump-test.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2026 Viliam Lejcik.  Licensed under the MIT License.
+// SPDX-License-Identifier: MIT
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
